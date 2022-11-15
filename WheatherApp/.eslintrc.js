ea1a42c5772271c168b6a4d8dad5658c38b89398
edug <gedu@users.noreply.github.com>
2022-11-15 15:51:50 +0100

    Setup Prettier and eslint (#1)
    Setup Prettier and eslint

--- WheatherApp/.eslintrc.js
+++ WheatherApp/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community/eslint-config'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'typescript-sort-keys', 'import'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
@@ -13,4 +13,66 @@ module.exports = {
       },
     },
   ],
+  rules: {
+    'typescript-sort-keys/interface': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'react-native',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: ['const', 'expression'],
+        next: 'return',
+      },
+      {
+        blankLine: 'always',
+        prev: ['cjs-import', 'import'],
+        next: '*',
+      },
+      {
+        blankLine: 'any',
+        prev: ['cjs-import', 'import'],
+        next: ['cjs-import', 'import'],
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'typeAlias',
+        format: ['PascalCase'],
+      },
+    ],
+    'import/no-cycle': [
+      'error',
+      {
+        maxDepth: 1,
+        ignoreExternal: true,
+      },
+    ],
+    'import/exports-last': 2,
+    'react-native/no-unused-styles': 2,
+    quotes: ['error', 'single'],
+  },
 };
